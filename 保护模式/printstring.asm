5e6671b16ab9a861e122cf65c20235e8df95773f
mingchaoxin <17362681829@163.com>
2022-04-23 20:46:37 +0800

    第三次提交

---
+++ 保护模式/printstring.asm
@@ -0,0 +1,43 @@
+NUL equ 0x00
+SETCHAR equ 0x07
+VIDEOMEM equ 0xb800
+STRINGLEN equ 0xffff
+
+
+section align=16 vstart=0x7c00
+    mov si,SayHello
+    xor di,di;初始化
+    call PrintString
+    mov si,SayByeBye
+    call PrintString
+    jmp End
+
+PrintString:
+    .setup:
+    mov ax,VIDEOMEM
+    mov es,ax
+    
+    mov bh,SETCHAR;字符的属性
+    mov cx,STRINGLEN
+
+    .printchar:
+    mov bl,[ds:si]
+    inc si
+    mov [es:di],bl
+    inc di
+    mov [es:di],bh
+    inc di
+    or bl, NUL
+    jz .return
+    loop .printchar
+
+    .return:
+    ret
+
+SayHello db 'Hi there.I am Coding Master!'
+         db 0x00
+SayByeBye db 'I thinl you can handle it,bye!'
+          db 0x00        
+End:    jmp End
+times 510-($-$$) db 0
+                 db 0x55, 0xaa
